5a048db74f03017dd90bbcf1db52360b5c965901
d3molite <42088225+d3molite@users.noreply.github.com>
2018-09-21 17:33:37 +0200

    dann halt nicht

--- faqbot.py
+++ faqbot.py
@@ -388,7 +388,7 @@ async def on_ready():
     print(bot.user.id)
     print('------')
     await GameChanger()
-    await StateCheck()
+    #await StateCheck()
     global dhserv, mirn, megauser, admin, manager, faqmsgchan, faqdm, internal
     dhserv = fetchServer(137246928227270656)
     mirn = dcf.fetchEmoji(dhserv, mirnemoji)
