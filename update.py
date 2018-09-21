b28428547008dd1fc76317cffb510c2816249070
d3molite <42088225+d3molite@users.noreply.github.com>
2018-09-21 15:13:06 +0200

    updated the update module
    bla

--- update.py
+++ update.py
@@ -4,6 +4,6 @@ import sys
 print("Waiting for Bot to Shut down.")
 time.sleep(10)
 print("Pulling Update")
-subprocess.call("git pull https://github.com/d3molite/faqbot")
+subprocess.call("git pull https://github.com/d3molite/faqbot", shell=True)
 time.sleep(10)
 subprocess.Popen([sys.executable, "./faqbot.py"], shell=True)
