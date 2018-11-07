8fd9e616ec7b0e99b0a8c673c03eee5b09e50c45
d3molite <42088225+d3molite@users.noreply.github.com>
2018-11-07 16:51:36 +0100

    even lower weight
    maybe now dumb questions will stop

--- languageki.py
+++ languageki.py
@@ -14,7 +14,7 @@ topics = ["Nicht Erkannt","Platzreservierung","Pavillon","Turniere", "Zapfanlage
 
 
 def process(message):
-    qweight = 2
+    qweight = 1
     qscore = 0
     qperc = 0.0
     if "?" in message:
