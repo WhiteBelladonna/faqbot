160bc6a1a6579e6d332c68790ecba0d21a582ffd
d3molite <42088225+d3molite@users.noreply.github.com>
2018-11-08 18:47:21 +0100

    keywords

--- languageki.py
+++ languageki.py
@@ -7,7 +7,7 @@ keywrd = [
         ],
         [
             ["reservierung", "sitzplatzreservierung", "platzwahl", "seating", "sitzplatz", "sitzplätze", "platzreservierung"],
-            ["wann", "beginnt", "eigentlich", "geht", "datum", "los", "startet", "neues", "gibts", "was"]
+            ["wann", "beginnt", "eigentlich", "geht", "datum", "los", "startet", "neues", "gibts", "was", "stattfindet"]
         ],
         [
             ["pavillon", "pavillion", "zelt", "überdachung"],
